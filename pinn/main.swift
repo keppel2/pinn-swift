de44bf0932d749ff7acc85c1b6a38373d97d93b5
Ryan Keppel <ryankeppel@M-2.local>
2019-10-19 18:36:04 -0700

    Some initial antlr stuff.

--- pinn/main.swift
+++ pinn/main.swift
@@ -10,7 +10,7 @@ import Foundation
 import Antlr4
 
 var myinput = """
-func main() { 2 3
+func main() {
 print(5);
 }
 """
@@ -21,7 +21,15 @@ var stream = CommonTokenStream(lexer)
 var parser = try? PinnParser(stream)
 
 var tree = try? parser!.file()
-    
 
 print("Hello, World!")
+class PVal {
+    
+}
 
+func visitFile(actx: PinnParser.FileContext) {
+    for child in actx.function() {
+        print(child.getText())
+    }
+}
+visitFile(actx:tree!)
