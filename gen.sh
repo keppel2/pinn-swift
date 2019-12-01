18cc7e6c8e4b6b19ff0cb824079e180820eb462d
Ryan Keppel <ryankeppel@M-2.local>
2019-12-01 13:56:23 -0800

    m

--- gen.sh
+++ gen.sh
@@ -1,2 +1,1 @@
 java -jar /usr/local/lib/antlr.jar -no-listener -Dlanguage=Swift -message-format gnu -Werror Pinn.g4
-java -jar /usr/local/lib/antlr.jar -no-listener -Dlanguage=Swift -message-format gnu -Werror Pinn.g4
