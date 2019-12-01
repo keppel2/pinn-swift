19ec7c44f44f382323a4210bb9cc94bce97fa6fb
Ryan Keppel <ryankeppel@M-2.local>
2019-12-01 13:55:33 -0800

    Add generate.

---
+++ gen.sh
@@ -0,0 +1,2 @@
+java -jar /usr/local/lib/antlr.jar -no-listener -Dlanguage=Swift -message-format gnu -Werror Pinn.g4
+java -jar /usr/local/lib/antlr.jar -no-listener -Dlanguage=Swift -message-format gnu -Werror Pinn.g4
