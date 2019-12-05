c9b8788ddf35bca138dd805d733c5a6d62fa127b
Ryan Keppel <ryankeppel@M-2.local>
2019-12-04 22:57:48 -0800

    Use gen.sh to put swift code in src/

--- gen.sh
+++ gen.sh
@@ -1,1 +1,1 @@
-java -jar /usr/local/lib/antlr.jar -no-listener -Dlanguage=Swift -message-format gnu -Werror Pinn.g4
+java -jar /usr/local/lib/antlr.jar -no-listener -Dlanguage=Swift -message-format gnu -Werror -o src Pinn.g4
