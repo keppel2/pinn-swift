ef6ed2321011b844eb6f87d65399fa34f89770a1
Ryan Keppel <ryankeppel@M-2.local>
2019-12-04 22:56:33 -0800

    Move swift files to src.

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "pinn",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(path: "antlr4/runtime/Swift")
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "pinn",
+            dependencies: ["Antlr4"],
+            path: "src")
+    ]
+)
