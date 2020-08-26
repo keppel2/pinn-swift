d6004a37e0593dfc50a36f21d4305675ab028d8f
Ryan Keppel <ryankeppel@M-2.local>
2020-08-25 21:32:01 -0700

    7

--- src/main.swift
+++ src/main.swift
@@ -2,14 +2,8 @@ import os
 import Foundation
 //let x = Array.init(repeating: 1, count: 0xFFFFF_FFFF)
 let fm = FileManager.default
-let str: String = fm.currentDirectoryPath
-print(str)
-print(str)
-
-print(str)
 //str.write
 
-print("here")
 //exit(0)
 
 do {
